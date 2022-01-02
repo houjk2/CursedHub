e19d69aa6764d35e07fca45d5bce0838ecfc8a62
houjk2 <96595333+houjk2@users.noreply.github.com>
2022-01-02 22:18:43 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 4581966615 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/houjk2/CursedHub/main/scripts/Anomic.lua"))()
+elseif game.PlaceId == 286090429 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/houjk2/CursedHub/main/scripts/Arsenal.lua"))()
 end
