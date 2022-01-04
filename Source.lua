6448e13909f9ec69a76bd3cb0ec1501d4e9fd44d
houjk2 <96595333+houjk2@users.noreply.github.com>
2022-01-04 20:21:04 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -2,6 +2,6 @@ if game.PlaceId == 4581966615 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/houjk2/CursedHub/main/scripts/Anomic.lua"))()
 elseif game.PlaceId == 286090429 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/houjk2/CursedHub/main/scripts/Arsenal.lua"))()
-elseif game.PlaceId == 00281782 then
+elseif game.PlaceId == 606849621 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/houjk2/CursedHub/main/scripts/Jailbreak.lua"))()
 end
